bc29ba06125a94d8f09bb608897bda1cb57e3647
YanshengLi0228 <ylii0723@student.monash.edu>
2026-03-14 15:23:10 +1100

    Add UVGuard logo to navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,14 @@
 import { Link } from "react-router-dom";
+import logo from "../assets/logo.png";
 
 function Navbar() {
   return (
     <nav className="navbar">
-      <div className="brand">UVGuard</div>
-
+      {/* <div className="brand">UVGuard</div> */}
+      <Link to="/" className="brand">
+        <img src={logo} alt="UVGuard Logo" className="nav-logo" />
+        UVGuard
+      </Link>
       <div className="nav-links">
         <Link to="/">Home</Link>
         <Link to="/uv-check">UV Check</Link>
